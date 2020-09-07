a64e247101c9d7fb32ead366bbd615bf326864ca
Nava <navag93@gmail.com>
2020-09-07 21:50:41 +0300

    Initialize Django project

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -176,3 +177,5 @@ DATABASES = {
         "ENGINE": 'django.contrib.gis.db.backends.postgis',
     }
 }
+
+django_heroku.settings(locals())
