dd154f7a373f31a2286f99c49b54372a29d469ac
Nava <navag93@gmail.com>
2020-09-08 13:16:04 +0300

    gdal issues

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -179,7 +179,7 @@ DATABASES = {
     }
 }
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
 
 django_heroku.settings(locals())
