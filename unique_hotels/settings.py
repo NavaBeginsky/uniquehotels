ca6e4e2aeb5872834e23806706ba521428446c58
Nava <navag93@gmail.com>
2020-09-08 12:13:10 +0300

    gdal issues

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -179,7 +179,11 @@ DATABASES = {
     }
 }
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+# GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
+GEOS_LIBRARY_PATH = '/app/.geodjango/geos/lib/libgeos_c.so'
+
+GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
 
 django_heroku.settings(locals())
