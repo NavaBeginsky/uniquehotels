7cbbed4002b111759e36a5337763e7d2514abee3
Nava <navag93@gmail.com>
2020-09-09 14:29:34 +0300

    newmigrations

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('update-profile/photo/', views.UpdateProfilePhoto.as_view(), name='update_profile_pic'),
     path('change-password/', views.UpdatePassword.as_view(), name='change_password'),
     path('delete-account/', views.DeleteAccount.as_view(), name='delete_account'),
-    path('<str:username>/', views.UserProfile.as_view(), name='profile'), 
+    # path('<str:username>/', views.UserProfile.as_view(), name='profile'), 
     path('post/ajax/like/', views.likeUnlike, name='like_unlike')
 ]
