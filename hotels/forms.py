7c0e2aa13b2a3d6296621414e051941a49947087
Nava <navag93@gmail.com>
2020-09-09 15:33:11 +0300

    settings moved

--- hotels/forms.py
+++ hotels/forms.py
@@ -1,27 +1,27 @@
 from django import forms
-# from .models import Categories, Amenities
+from .models import Categories, Amenities
 
-# class CategoryForm(forms.Form):
-#     category_options = Categories.objects.all().values_list('pk', 'name')
-#     categories = forms.MultipleChoiceField(
-#         required=False,
-#         widget=forms.CheckboxSelectMultiple,
-#         choices=category_options
-#     )
+class CategoryForm(forms.Form):
+    category_options = Categories.objects.all().values_list('pk', 'name')
+    categories = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=category_options
+    )
 
-# class AmenityForm(forms.Form):
-#     amenity_options = Amenities.objects.all().values_list('pk', 'name')
-#     amenities = forms.MultipleChoiceField(
-#         required=False,
-#         widget=forms.CheckboxSelectMultiple,
-#         choices=amenity_options
-#     )
+class AmenityForm(forms.Form):
+    amenity_options = Amenities.objects.all().values_list('pk', 'name')
+    amenities = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=amenity_options
+    )
 
-# class LocationForm(forms.Form):
-#     lat = forms.FloatField(required=False)
-#     lon = forms.FloatField(required=False)
-#     radius = forms.IntegerField(required=False)
+class LocationForm(forms.Form):
+    lat = forms.FloatField(required=False)
+    lon = forms.FloatField(required=False)
+    radius = forms.IntegerField(required=False)
 
-#     def __init__(self, *args, **kwargs):
-#         super(LocationForm, self).__init__(*args, **kwargs)
-#         self.fields['radius'].label = "Kilometers"
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        self.fields['radius'].label = "Kilometers"
