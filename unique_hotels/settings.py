a884804aec90e97226e6ce8cb97a6118098ec67d
Nava <navag93@gmail.com>
2020-09-09 19:56:09 +0300

    media file storage

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -173,6 +173,7 @@ if os.getenv('SECRET_KEY'):
 
     import dj_database_url
     DATABASES = { 'default' : dj_database_url.config()}
+    DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.postgis"
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
