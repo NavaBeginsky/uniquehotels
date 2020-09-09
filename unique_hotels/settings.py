2126e5957b8d680afe827834c82f5ec9d03331ff
Nava <navag93@gmail.com>
2020-09-09 13:42:28 +0300

    newmigrations

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -148,9 +148,19 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # #Load local settings
 if os.getenv('SECRET_KEY'):
     SECRET_KEY = os.getenv('SECRET_KEY')
+
+    import dj_database_url
+    DATABASES = { 'default' : dj_database_url.config()}
+
+    DATABASES = {
+        "default": {
+            "ENGINE": 'django.contrib.gis.db.backends.postgis',
+        }
+    }
+
 else:
     try:
-    from .local_settings import *
+        from .local_settings import *
     except ImportError:
         raise Exception('A local_settings.py file is required to run this project')
 
@@ -167,19 +177,9 @@ MAP_WIDGETS = {
 # Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-
-DATABASES = { 'default' : dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-DATABASES = {
-    "default": {
-        "ENGINE": 'django.contrib.gis.db.backends.postgis',
-    }
-}
 
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
