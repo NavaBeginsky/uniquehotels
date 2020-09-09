a1f94c75d391a052b255d9227ef793400206a4c5
Nava <navag93@gmail.com>
2020-09-09 22:43:26 +0300

    GOOGLE API heroku

--- unique_hotels/settings.py
+++ unique_hotels/settings.py
@@ -177,6 +177,7 @@ if os.getenv('SECRET_KEY'):
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    GOOGLE_API = os.environ.get('GOOGLE_API')
 
 
 else: #Load local settings
